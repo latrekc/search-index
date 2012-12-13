a787ebf1392909df7829d8b2a28921dc0ede43a9
latrekc <latrekc@gmail.com>
2012-12-13 21:28:04 +0400

    letter & punto variants; move all function  to class — minimize interface

--- test.js
+++ test.js
@@ -1,65 +1,187 @@
+if(!Array.prototype.unique) {
+	Array.prototype.unique = function() {
+		var used = {};
+
+		return this.filter(function(a) {
+			Array.prototype.unique_count++;
+
+			if(used[a] === undefined) {
+				used[a] = true;
+				return true;
+
+			} else {
+				return false
+			}
+		});
+	}
+	Array.prototype.unique_count = 0;
+}
+
+function SearchIndex(data) {
+	var _punto = {
+		"й":"q",
+		"ц":"w",
+		"у":"e",
+		"к":"r",
+		"е":"t",
+		"н":"y",
+		"г":"u",
+		"ш":"i",
+		"щ":"o",
+		"з":"p",
+		"х":"[",
+		"ъ":"]",
+		"ф":"a",
+		"ы":"s",
+		"в":"d",
+		"а":"f",
+		"п":"g",
+		"р":"h",
+		"о":"j",
+		"л":"k",
+		"д":"l",
+		"ж":";",
+		"э":"'",
+		"ё":"\\",
+		"я":"z",
+		"ч":"x",
+		"с":"c",
+		"м":"v",
+		"и":"b",
+		"т":"n",
+		"ь":"m",
+		"б":",",
+		"ю":".",
+
+		"q":"й",
+		"w":"ц",
+		"e":"у",
+		"r":"к",
+		"t":"е",
+		"y":"н",
+		"u":"г",
+		"i":"ш",
+		"o":"щ",
+		"p":"з",
+		"[":"х",
+		"]":"ъ",
+		"a":"ф",
+		"s":"ы",
+		"d":"в",
+		"f":"а",
+		"g":"п",
+		"h":"р",
+		"j":"о",
+		"k":"л",
+		"l":"д",
+		";":"ж",
+		"'":"э",
+		"ё":"\\",
+		"z":"я",
+		"x":"ч",
+		"c":"с",
+		"v":"м",
+		"b":"и",
+		"n":"т",
+		"m":"ь",
+		",":"б",
+		".":"ю",
+	};
+
+	var _index = {};
+
+	// init index
+	data.forEach(function(str, id) {
+		put(_index, str, 0, id);
+	});
+
+
+	function lettersAtPos(str, pos) {
+		var variants = [];
+		var letter = str.charAt(pos);
+
+		variants.push(letter);
+
+		var punto = _punto[letter];
+		if(punto !== undefined) {
+			variants.push(punto);
+		}
+
+		return variants;
+	}
+
+	function put(cache, str, pos, id) {
+		lettersAtPos(str, pos).forEach(function(letter) {
+			if(cache[letter] === undefined) {
+				cache[letter] = {
+					ids: [],
+					cache: {}
+				};
+			}
+
+			cache[letter].ids.push(id);
+
+			if(pos + 1 < str.length) {
+				put(cache[letter].cache, str, pos + 1, id);
+			}
+		})
+	}
+
+	function find(cache, str, pos) {
+		var results = [];
+
+		lettersAtPos(str, pos).forEach(function(letter) {
+			if(cache[letter] !== undefined) {
+				var res;
+
+				if(pos + 1 == str.length) {
+					res = cache[letter].ids;
+				} else {
+					res = find(cache[letter].cache, str, pos + 1);
+				}
+
+				results.push.apply(results, res);
+			}
+		});
+
+		return results;
+	}
+
+	return {
+		find: function(str) {
+			return find(_index, str, 0).unique().map(function(id) {
+				return data[id];
+			});
+		}
+	}
+}
+
 var data = [
 	'a',
 	'ab',
 	'abc',
 	'abcd',
-	'abcde',
+
+	'ф',
+	'фи',
+	'фис',
+	'фисв',
 
 	'а',
 	'аб',
 	'абв',
 	'абвг',
-	'абвгд'
 ];
 
 
-var mainCache = {};
+var index = SearchIndex(data);
 
-function putInCache(cache, str, pos, id) {
-	var letter = str.charAt(pos);
-
-	if(cache[letter] === undefined) {
-		cache[letter] = {
-			ids: [],
-			cache: {}
-		};
-	}
-
-	cache[letter].ids.push(id);
-
-	if(pos + 1 < str.length) {
-		putInCache(cache[letter].cache, str, pos + 1, id);
-	}
-}
-
-function findInCache(cache, query, pos) {
-	var letter = query.charAt(pos);
-
-	if(cache[letter] !== undefined) {
-		if(pos + 1 == query.length) {
-			return cache[letter].ids;
-
-		} else {
-			return findInCache(cache[letter].cache, query, pos + 1);
-		}
-
-	} else {
-		return [];
-	}
-}
-
-data.forEach(function(str, id) {
-	putInCache(mainCache, str, 0, id)
+data.forEach(function(str, i) {
+	console.log(i, str);
 })
-
-
-console.log(data);
 console.log('')
 
-console.log(require('util').inspect(mainCache, false, null));
-console.log('')
-
-console.log(findInCache(mainCache, 'abc', 0));
+console.log(index.find('abc'));
 console.log('')
 
 
